f0a120fcc0f36744f474d581f286406efd8e3ab8
Talal Tabia <talal.tabia.one@gmail.com>
2019-08-26 01:01:40 +0200

    hide access token and readd client to history

---
+++ src/main/java/com/ttabia/ubat/client/Web3jClient.java
@@ -0,0 +1,80 @@
+package com.ttabia.ubat.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class Web3jClient {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(Web3jClient.class);
+
+    private static final String INFURA_MAINNET = "https://mainnet.infura.io/";
+
+    private Web3j web3;
+
+    @Autowired
+    private Environment environment;
+
+    public String getClientVersion() throws Exception {
+
+        try {
+            Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().send();
+            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+            return clientVersion;
+        } catch (IOException e) {
+            LOGGER.error("Internal error", e);
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public BigInteger getBalance(String address) throws Exception {
+
+        try {
+            BigInteger blockNumber = getBlockNumber();
+            BigInteger balance = web3.ethGetBalance(address, DefaultBlockParameter.valueOf(blockNumber)).sendAsync().get().getBalance();
+            return balance;
+        } catch (InterruptedException | ExecutionException e) {
+            LOGGER.error("Internal error", e);
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public BigInteger getBlockNumber() throws Exception {
+
+        try {
+            BigInteger blockNumber = web3.ethBlockNumber().send().getBlockNumber();
+            return blockNumber;
+        } catch (IOException e) {
+            LOGGER.error("Internal error", e);
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @PostConstruct
+    public void connect() {
+
+        LOGGER.info("Building client with infura main net...");
+        String projectId = environment.getProperty("infura-project-id");
+        web3 = Web3j.build(new HttpService(INFURA_MAINNET + projectId));  // defaults to http://localhost:8545/
+    }
+
+    @PreDestroy
+    public void close() {
+
+        web3.shutdown();
+        LOGGER.info("terminating web3j client.");
+    }
+}
