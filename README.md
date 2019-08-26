58b416b1fe13b77ec26967c77591b34be6bdfde3
Talal Tabia <talal.tabia.one@gmail.com>
2019-08-26 23:48:31 +0200

    add path variables | update readme

--- README.md
+++ README.md
@@ -6,8 +6,38 @@
 
 #### deliver address info 
 
-curl -i -X GET  "http://localhost:8080/info/ethereum"
+```shell script
+$ curl -i -X GET  "http://localhost:8080/info/ethereum/0xbbe79f7d50e4ff6e8ca61082a8740c86e873b3b8"
+```
+```json
+{
+  "clientVersion":"Geth/v1.8.27-omnibus-c94f741c/linux-amd64/go1.11.1",
+  "address":"0xbbe79f7d50e4ff6e8ca61082a8740c86e873b3b8",
+  "blockNumber":"8428270",
+  "balance":"0.1766469833609384 Ξ"
+}
+```
+HTTP/1.1 200<br />
+Content-Type: application/json;charset=UTF-8<br />
+Transfer-Encoding: chunked<br />
+Date: Mon, 26 Aug 2019 21:38:37 GMT
 
 #### deliver transaction details
 
-curl -i -X GET  "http://localhost:8080/transactionDetail/ethereum"
+```shell script
+$ curl -i -X GET  "http://localhost:8080/transactionDetail/ethereum/0xd014161ce38549f79ceee8cfbcede920726fe8b50ea748b72a1cff65330158c4"
+```
+```json
+{
+  "value":25368873531990629,
+  "from":"0xea674fdde714fd979de3edf0f56aa9716b898ec8",
+  "to":"0xbbe79f7d50e4ff6e8ca61082a8740c86e873b3b8",
+  "block":4836332,
+  "gasPrice":1000000000,
+  "gas":50000
+}
+```
+HTTP/1.1 200<br />
+Content-Type: application/json;charset=UTF-8<br />
+Transfer-Encoding: chunked<br />
+Date: Mon, 26 Aug 2019 21:39:18 GMT
--- src/main/java/com/ttabia/ubat/controller/UbatController.java
+++ src/main/java/com/ttabia/ubat/controller/UbatController.java
@@ -6,27 +6,25 @@ import com.ttabia.ubat.service.UbatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class UbatController {
 
-    private static final String ADDRESS = "0xbbe79f7d50e4ff6e8ca61082a8740c86e873b3b8";
-    private static final String TRANSACTION_HASH = "0xd014161ce38549f79ceee8cfbcede920726fe8b50ea748b72a1cff65330158c4";
-
     @Autowired
     private UbatService ubatService;
 
-    @GetMapping("/info/ethereum")
-    public ResponseEntity<InfoDto> info() throws Exception {
+    @GetMapping("/info/ethereum/{address}")
+    public ResponseEntity<InfoDto> info(@PathVariable String address) throws Exception {
 
-        return ResponseEntity.ok(ubatService.getInfo(ADDRESS));
+        return ResponseEntity.ok(ubatService.getInfo(address));
     }
 
-    @GetMapping("/transactionDetail/ethereum")
-    public ResponseEntity<TransactionDto> transactionDetail() throws Exception {
+    @GetMapping("/transactionDetail/ethereum/{transactionHash}")
+    public ResponseEntity<TransactionDto> transactionDetail(@PathVariable String transactionHash) throws Exception {
 
-        return ResponseEntity.ok(ubatService.getTransactionDetail(TRANSACTION_HASH));
+        return ResponseEntity.ok(ubatService.getTransactionDetail(transactionHash));
     }
 }
